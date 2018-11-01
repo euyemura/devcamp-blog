2da9f6bb8010eaecfc11aee517763b5b78875d1c
euyemura <ea.uyemura@gmail.com>
2018-11-01 08:20:25 -0700

    craeted views and looped over portfolioitems

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,7 @@
 class PortfoliosController < ApplicationController
+  def index #good for listing out the number of items, also, most things need an index.
+    @portfolio_items = Portfolio.all
+    #the above allows us to make the portfolio items avaialable to view.
+    
+  end
 end
